24e8561d0872854670ab4b12f7d94781b1ef5bde
Canux Cheng <canux.cheng@arm.com>
2023-06-08 15:40:27 +0800

    update

--- tracking.ino
+++ tracking.ino
@@ -1,27 +1,6 @@
-/**
-* @par Copyright (C): 2010-2019, Shenzhen Yahboom Tech
-* @file         tracking.c
-* @author       Danny
-* @version      V1.0
-* @date         2017.07.26
-* @brief        巡线实验
-* @details
-* @par History  见如下说明
-*
-*/
-int Left_motor_go = 8;        //左电机前进 AIN1
-int Left_motor_back = 7;      //左电机后退 AIN2
 
-int Right_motor_go = 2;       //右电机前进 BIN1
-int Right_motor_back = 4;     //右电机后退 BIN2
-
-int Left_motor_pwm = 6;       //左电机控速 PWMA
-int Right_motor_pwm = 5;      //右电机控速 PWMB
-
-// int key = A0;                 //定义按键为arduino的模拟口A0
-#include "MecanumCar_v2.h"
-mecanumCar mecanumCar(3, 2); 
-
+#include "MecanumCar_v4.h"
+mecanumCar mecanumCar(3, 2);  //sda-->D3,scl-->D2
 
 //循迹红外引脚定义
 //TrackSensorLeftPin1 TrackSensorLeftPin2 TrackSensorRightPin1 TrackSensorRightPin2
@@ -37,28 +16,11 @@ int TrackSensorLeftValue2;
 int TrackSensorRightValue1;
 int TrackSensorRightValue2;
 
-/**
-* Function       setup
-* @author        Danny
-* @date          2017.07.25
-* @brief         初始化配置
-* @param[in]     void
-* @retval        void
-* @par History   无
-*/
+
 void setup()
 {
   Serial.begin(9600);
   Serial.println("setup ......");
-
-  // //初始化电机驱动IO口为输出方式
-  // pinMode(Left_motor_go, OUTPUT);
-  // pinMode(Left_motor_back, OUTPUT);
-  // pinMode(Right_motor_go, OUTPUT);
-  // pinMode(Right_motor_back, OUTPUT);
-
-  //定义按键接口为输入接口
-  // pinMode(key, INPUT);
 
   //定义四路循迹红外传感器为输入接口
   pinMode(TrackSensorLeftPin1, INPUT);
@@ -66,219 +28,15 @@ void setup()
   pinMode(TrackSensorRightPin1, INPUT);
   pinMode(TrackSensorRightPin2, INPUT);
 
-  //按键初始化为高电平
-  // digitalWrite(key, HIGH);
-
   //四路循迹红外传感器初始化为高电平
   digitalWrite(TrackSensorLeftPin1, HIGH);
   digitalWrite(TrackSensorLeftPin2, HIGH);
   digitalWrite(TrackSensorRightPin1, HIGH);
   digitalWrite(TrackSensorRightPin2, HIGH);
 
-  //调用按键扫描函数
-  // key_scan();
   mecanumCar.Init();  //初始化麦轮车驱动
 }
 
-// /**
-// * Function       run
-// * @author        Danny
-// * @date          2017.07.26
-// * @brief         小车前进
-// * @param[in1]    left_speed:左轮速度
-// * @param[in2]    right_speed:右轮速度
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// // void run(int left_speed, int right_speed)
-// // {
-// //   Serial.println("run...");
-// //   //左电机前进
-// //   digitalWrite(Left_motor_go, HIGH);   //左电机前进使能
-// //   digitalWrite(Left_motor_back, LOW);  //左电机后退禁止
-// //   analogWrite(Left_motor_pwm, left_speed );
-
-// //   //右电机前进
-// //   digitalWrite(Right_motor_go, HIGH);  //右电机前进使能
-// //   digitalWrite(Right_motor_back, LOW); //右电机后退禁止
-// //   analogWrite(Right_motor_pwm, right_speed);
-// // }
-
-// /**
-// * Function       brake
-// * @author        Danny
-// * @date          2017.07.25
-// * @brief         小车刹车
-// * @param[in]     time:延时时间
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// void brake(int time)
-// {
-//   digitalWrite(Left_motor_go, LOW);
-//   digitalWrite(Left_motor_back, LOW);
-//   digitalWrite(Right_motor_go, LOW);
-//   digitalWrite(Right_motor_back, LOW);
-//   delay(time * 100);
-// }
-
-// /**
-// * Function       left
-// * @author        Danny
-// * @date          2017.07.25
-// * @brief         小车左转(左轮不动，右轮前进)
-// * @param[in1]    left_speed:左轮速度
-// * @param[in2]    right_speed:右轮速度
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// void left(int left_speed, int right_speed)
-// {
-//   //左电机停止
-//   digitalWrite(Left_motor_go, LOW);    //左电机前进禁止
-//   digitalWrite(Left_motor_back, LOW);  //左电机后退禁止
-//   analogWrite(Left_motor_pwm, left_speed);
-
-//   //右电机前进
-//   digitalWrite(Right_motor_go, HIGH);  //右电机前进使能
-//   digitalWrite(Right_motor_back, LOW); //右电机后退禁止
-//   analogWrite(Right_motor_pwm, right_speed);
-// }
-
-// /**
-// * Function       right
-// * @author        Danny
-// * @date          2017.07.25
-// * @brief         小车右转(右轮不动，左轮前进)
-// * @param[in1]    left_speed:左轮速度
-// * @param[in2]    right_speed:右轮速度
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// void right(int left_speed, int right_speed)
-// {
-//   //左电机前进
-//   digitalWrite(Left_motor_go, HIGH);   //左电机前进使能
-//   digitalWrite(Left_motor_back, LOW);  //左电机后退禁止
-//   analogWrite(Left_motor_pwm, left_speed);
-
-//   //右电机停止
-//   digitalWrite(Right_motor_go, LOW);   //右电机前进禁止
-//   digitalWrite(Right_motor_back, LOW); //右电机后退禁止
-//   analogWrite(Right_motor_pwm, right_speed);
-// }
-
-// /**
-// * Function       spin_left
-// * @author        Danny
-// * @date          2017.07.25
-// * @brief         小车原地左转(左轮后退，右轮前进)
-// * @param[in1]    left_speed:左轮速度
-// * @param[in2]    right_speed:右轮速度
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// void spin_left(int left_speed, int right_speed)
-// {
-//   //左电机后退
-//   digitalWrite(Left_motor_go, LOW);     //左电机前进禁止
-//   digitalWrite(Left_motor_back, HIGH);  //左电机后退使能
-//   analogWrite(Left_motor_pwm, left_speed);
-
-//   //右电机前进
-//   digitalWrite(Right_motor_go, HIGH);  //右电机前进使能
-//   digitalWrite(Right_motor_back, LOW); //右电机后退禁止
-//   analogWrite(Right_motor_pwm, right_speed);
-// }
-
-// /**
-// * Function       spin_right
-// * @author        Danny
-// * @date          2017.07.25
-// * @brief         小车原地右转(右轮后退，左轮前进)
-// * @param[in1]    left_speed:左轮速度
-// * @param[in2]    right_speed:右轮速度
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// void spin_right(int left_speed, int right_speed)
-// {
-//   //左电机前进
-//   digitalWrite(Left_motor_go, HIGH);    //左电机前进使能
-//   digitalWrite(Left_motor_back, LOW);   //左电机后退禁止
-//   analogWrite(Left_motor_pwm, left_speed);
-
-//   //右电机后退
-//   digitalWrite(Right_motor_go, LOW);    //右电机前进禁止
-//   digitalWrite(Right_motor_back, HIGH); //右电机后退使能
-//   analogWrite(Right_motor_pwm, right_speed);
-// }
-
-// /**
-// * Function       back
-// * @author        Danny
-// * @date          2017.07.25
-// * @brief         小车后退
-// * @param[in]     time：延时时间
-// * @param[out]    void
-// * @retval        void
-// * @par History   无
-// */
-// void back(int time)
-// {
-//   //左电机后退
-//   digitalWrite(Left_motor_go, LOW);     //左电机前进禁止
-//   digitalWrite(Left_motor_back, HIGH);  //左电机后退使能
-//   analogWrite(Left_motor_pwm, 40);
-
-//   //右电机后退
-//   digitalWrite(Right_motor_go, LOW);    //右电机前进禁止
-//   digitalWrite(Right_motor_back, HIGH); //右电机后退使能
-//   analogWrite(Right_motor_pwm, 40);
-
-//   delay(time );
-// }
-
-/**
-* Function       key_scan
-* @author        Danny
-* @date          2017.07.25
-* @brief         按键检测(包含软件按键去抖)
-* @param[in]     void
-* @param[out]    void
-* @retval        void
-* @par History   无
-*/
-// void key_scan()
-// {
-//   while (digitalRead(key));       //当按键没有被按下一直循环
-//   while (!digitalRead(key))       //当按键被按下时
-//   {
-//     delay(10);	                  //延时10ms
-//     if (digitalRead(key)  ==  LOW)//第二次判断按键是否被按下
-//     {
-//       delay(100);
-//       while (!digitalRead(key));  //判断按键是否被松开
-//     }
-//   }
-// }
-
-/**
-* Function       loop
-* @author        Danny
-* @date          2017.07.25
-* @brief         先调用setup初始化配置里面的按键扫描函数，
-*                循迹模式开启
-* @param[in]     void
-* @retval        void
-* @par History   无
-*/
 void loop()
 {
   Serial.println("loop......");
@@ -292,80 +50,73 @@ void loop()
   Serial.println(TrackSensorLeftValue2);
   Serial.println(TrackSensorRightValue1);
   Serial.println(TrackSensorRightValue2);
-
   
-  //四路循迹引脚电平状态
-  // 0 0 X 0
-  // 1 0 X 0
-  // 0 1 X 0
-  //以上6种电平状态时小车原地右转，速度为250,延时80ms
-  //处理右锐角和右直角的转动
-  if ( (TrackSensorLeftValue1 == LOW || TrackSensorLeftValue2 == LOW) &&  TrackSensorRightValue2 == LOW)
+  //turn right
+  //  1100, 1110, 1101?  
+  if ((TrackSensorLeftValue1 == HIGH && TrackSensorLeftValue2 == HIGH && TrackSensorRightValue2 == LOW) || (TrackSensorLeftValue1 == HIGH && TrackSensorLeftValue2 == HIGH && TrackSensorRightValue1 == LOW))
   {
-    mecanumCar.Turn_Right();//右转
-    delay(80);
-    // spin_right(250, 250);
-    // delay(80);
+    Serial.println("right");
+    mecanumCar.Turn_Right(15, 15, 15, 15); //右转
+  }
+  //1010???
+  if ((TrackSensorLeftValue1 == HIGH &&  TrackSensorRightValue1 == HIGH && TrackSensorRightValue2 == LOW))
+  {
+    Serial.println("right");
+    mecanumCar.Turn_Right(15, 15, 15, 15); //右转
+  }
+  //1000
+  else if (TrackSensorLeftValue1 == HIGH && TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == LOW && TrackSensorRightValue2 == LOW)
+  {
+    Serial.println("right");
+    mecanumCar.Turn_Right(15, 15, 15, 15); //右转
+  }
+  // 0110, ,  0100?    0101?    
+  else if ((TrackSensorLeftValue1 == LOW && TrackSensorLeftValue2 == HIGH && TrackSensorRightValue1 == LOW) || (TrackSensorLeftValue1 == LOW && TrackSensorLeftValue2 == HIGH && TrackSensorRightValue2 == LOW))
+  {
+    Serial.println("right");
+    mecanumCar.Turn_Right(15, 15, 15, 15); //右转
+  }
+  //0010???    
+  else if (TrackSensorLeftValue1 == LOW && TrackSensorRightValue1 == HIGH  && TrackSensorRightValue2 == LOW)
+  {
+    Serial.println("right");
+    mecanumCar.Turn_Right(15, 15, 15, 15); //右转
+  }
+  // 0000
+  else if (TrackSensorLeftValue1 == LOW && TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == LOW && TrackSensorRightValue2 == LOW) 
+  {
+    Serial.println("right");
+    mecanumCar.Turn_Right(15, 15, 15, 15); //右转
+  }
+
+  // turn left
+  // 0011    0111       0010???     
+  else if (TrackSensorLeftValue1 == LOW && TrackSensorRightValue1 == HIGH && TrackSensorRightValue2 == HIGH)
+  {
+    Serial.println("left");
+    mecanumCar.Turn_Left(15, 15, 15, 15); // 右转
+  }
+  // 1011   1010???  
+  else if (TrackSensorLeftValue1 == HIGH && TrackSensorLeftValue1 == LOW && TrackSensorRightValue1 == HIGH && TrackSensorRightValue2 == HIGH)
+  {
+    Serial.println("left");
+    mecanumCar.Turn_Left(15, 15, 15, 15); // 右转
   }
 
   //四路循迹引脚电平状态
-  // 0 X 0 0       
-  // 0 X 0 1 
-  // 0 X 1 0       
-  //处理左锐角和左直角的转动
-  else if ( TrackSensorLeftValue1 == LOW && (TrackSensorRightValue1 == LOW ||  TrackSensorRightValue2 == LOW))
-  {
-    mecanumCar.Turn_Left(); //左转
-    delay(80);
-    // spin_left(250, 250);
-    // delay(80);
-  }
-
-  // 0 X X X
-  //最左边检测到
-  // else if ( TrackSensorLeftValue1 == LOW)
-  // {
-  //   spin_left(150, 150);
-  //   delay(2);
-  // }
-
-  // X X X 0
-  //最右边检测到
-  // else if ( TrackSensorRightValue2 == LOW )
-  // {
-  //   spin_right(150, 150);
-  //   delay(2);            //等待2秒
-  // }
-
-  //四路循迹引脚电平状态
-  // X 0 1 X
-  //处理左小弯
-  // else if ( TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == HIGH)
-  // {
-  //   left(0, 220);
-  // }
-
-  //四路循迹引脚电平状态
-  // X 1 0 X  
-  //处理右小弯
-  // else if (TrackSensorLeftValue2 == HIGH && TrackSensorRightValue1 == LOW)
-  // {
-  //   right(220, 0);
-  // }
-
-  //四路循迹引脚电平状态
-  // X 0 0 X
+  // 0001 , 1001,
   //处理直线
-  else if (TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == LOW)
+  else if (TrackSensorLeftValue2 == LOW && TrackSensorRightValue1 == LOW && TrackSensorRightValue2 == HIGH)
   {
-    // run(255, 255);
-    mecanumCar.Advance();
+    Serial.println("go go go");
+    mecanumCar.Advance(30, 30, 30, 30);
   }
-  else if (TrackSensorLeftValue1 == HIGH && TrackSensorLeftValue2 == HIGH && TrackSensorRightValue1 == HIGH && TrackSensorRightValue2 == HIGH)
-  {
-    mecanumCar.Stop();
-  }
-  //当为1 1 1 1时小车保持上一个小车运行状态
+  // 1111
+  // else if (TrackSensorLeftValue1 == HIGH && TrackSensorLeftValue2 == HIGH && TrackSensorRightValue1 == HIGH && TrackSensorRightValue2 == HIGH)
+  // {
+  //   Serial.println("stop");
+  //   mecanumCar.Stop();
+  // }
 }
 
 
